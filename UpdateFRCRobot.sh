8cbf928a284db3fb2d711bc4e772099196dac141
Jake <pi31415926535Jake@gmail.com>
2019-09-04 20:51:28 -0400

    Added a simple script to update frc_robot only.
    This copies the frc_robot folder only into the ~/catkin_ws/src/ folder.

---
+++ UpdateFRCRobot.sh
@@ -0,0 +1,17 @@
+cwd=$PWD
+echo "Currernt working directory:" $cwd
+
+
+# Copy necessary packages
+
+rm -rf ~/catkin_ws/src/frc_robot/
+cp -R $cwd/catkin_ws/src/frc_robot ~/catkin_ws/src/
+
+# Build everything
+
+cd ~/catkin_ws
+catkin_make
+source ~/.bashrc
+rospack list
+cd ~/catkin_ws/src/frc_robot
+roswtf
