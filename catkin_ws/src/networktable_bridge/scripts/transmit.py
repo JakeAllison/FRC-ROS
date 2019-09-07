c3d977f67c416eb42b42859ae3fbd9423b863c3a
Drew Williams <williams.r.drew@gmail.com>
2019-09-07 19:50:02 -0400

    can subscribe and unsubscribe with nt. Cant figure out types

--- catkin_ws/src/networktable_bridge/scripts/transmit.py
+++ catkin_ws/src/networktable_bridge/scripts/transmit.py
@@ -3,11 +3,13 @@
 import threading
 import rospy
 from networktables import NetworkTables
-from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Pose, Twist
 
 IP = 'localhost'
 
 ros_table = None
+
+subscribed_topics = dict()
 
 #Threading stuff for connecting to NT
 cond = threading.Condition()
@@ -20,12 +22,25 @@ def connectionListener(connected, info):
         cond.notify()
 
 def add_to_nt(data, topic):
-    ros_table.putRaw(topic, data)
+    ros_table.putString(topic, data)
 
 def transmit():
+    prev_command = ""
     while True:
-        ros_table.putNumber('testing', 5)
-        topics = rospy.get_published_topics()
+        command_control = ros_table.getString("command_control", "")
+
+        if command_control != prev_command:
+            print("Command is: " + command_control)
+            command_words = command_control.split()
+            if command_words[0] == "subscribe":
+                topic = rospy.Subscriber(command_words[1], Twist, add_to_nt, callback_args=command_words[1])
+                subscribed_topics = {command_words[1] : topic}
+                print(topic)
+            if command_words[0] == "unsubscribe":
+                print(subscribed_topics[command_words[1]])
+                subscribed_topics[command_words[1]].unregister()
+
+        prev_command = command_control
 
 if __name__ == '__main__':
     try:
@@ -40,13 +55,13 @@ if __name__ == '__main__':
         #Start transmitting ROS data to NT
         print("Connected!")
         ros_table = NetworkTables.getTable('ROS')
-
+        #Get subtable to appear!
+        ros_table.putNumber("test", 1)
         rospy.init_node('network_table_bridge', anonymous=False)
 
-        #Loop here and check if special NT entry is different than last, if so, subscribe to that topic?
 
-        my_topic = 'chatter'
-        rospy.Subscriber(my_topic, Pose, add_to_nt, callback_args=my_topic)
+        #my_topic = 'frc_diff_drive_controller/cmd_vel'
+        #rospy.Subscriber(my_topic, Twist, add_to_nt, callback_args=my_topic)
 
         transmit()
     except rospy.ROSInterruptException:
