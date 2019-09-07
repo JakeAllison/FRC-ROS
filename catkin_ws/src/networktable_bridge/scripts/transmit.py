ac73174d1d9c92f51e4f892d5d1b444a2f4a3f72
Drew Williams <williams.r.drew@gmail.com>
2019-09-07 17:25:02 -0400

    got ros->nt working. have to get a way to sub in NT

--- catkin_ws/src/networktable_bridge/scripts/transmit.py
+++ catkin_ws/src/networktable_bridge/scripts/transmit.py
@@ -3,8 +3,11 @@
 import threading
 import rospy
 from networktables import NetworkTables
+from geometry_msgs.msg import Pose
 
 IP = 'localhost'
+
+ros_table = None
 
 #Threading stuff for connecting to NT
 cond = threading.Condition()
@@ -16,8 +19,13 @@ def connectionListener(connected, info):
         notified[0] = True
         cond.notify()
 
+def add_to_nt(data, topic):
+    ros_table.putRaw(topic, data)
+
 def transmit():
-    pass
+    while True:
+        ros_table.putNumber('testing', 5)
+        topics = rospy.get_published_topics()
 
 if __name__ == '__main__':
     try:
@@ -31,6 +39,13 @@ if __name__ == '__main__':
 
         #Start transmitting ROS data to NT
         print("Connected!")
+        ros_table = NetworkTables.getTable('ROS')
+
+        rospy.init_node('network_table_bridge', anonymous=False)
+
+        my_topic = 'chatter'
+        rospy.Subscriber(my_topic, Pose, add_to_nt, callback_args=my_topic)
+
         transmit()
     except rospy.ROSInterruptException:
         pass
